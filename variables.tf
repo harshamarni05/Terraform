f7b0c708505048e024b1f2753397aaf1440a3b9d
Harsha <marnih2022@gmail.com>
2026-05-24 19:59:03 -0500

    Change

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "instance_type" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "my-terraform-bucket-harshamarni"
+  default     = "my-terraform-bucket-harshamarni-2026"
 }
 
 # The environment name — dev, staging, prod
